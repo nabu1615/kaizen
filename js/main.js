fb4e5d3c845433d219153d2dbb78d584257822f9
nabu1615 <AndresMac@MacBook-Pro-de-Javier.local>
2015-12-21 23:04:20 -0500

    fixed bugs related with header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 $(function() {
     var pull = $('.action_mobile'),
-        nav = $('.nav');
+        nav = $('.nav'),
+        homeIcon = $('.home-icon'),
+        wrapper = $('.wrapper');
 
    $(pull).on('click', function(e) {
     console.log("helo")
     e.preventDefault();
     $(this).toggleClass('active');
     $(nav).fadeToggle();
+    $(wrapper).fadeToggle();
     });
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>0)
+        {
+            $(homeIcon).fadeOut();
+            $('body').addClass("scrolling");
+        }
+        else
+        {
+            $(homeIcon).fadeIn();
+            $('body').removeClass("scrolling");
+        }
+    });
+
+
 });
 
