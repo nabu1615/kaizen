01476e44d126f916d5bf046cf54690730e3078b9
nabu1615 <AndresMac@MacBook-Pro-de-Javier.local>
2016-01-18 22:49:06 -0500

    desktop styling

--- js/main.js
+++ js/main.js
@@ -4,31 +4,44 @@ $(function() {
         homeIcon = $('.home-icon'),
         windowWidth = $(window).width(),
         active = 'active',
-        featuredWorkitem = ".featured-works .item .image img",
-        featuredWorkimageBox = ".featured-works .item .image",
+        featuredWorkitem = ".featured-works .item .image",
+        featuredWorkimageBox = ".featured-works .item .image img",
         wrapper = $('.wrapper'),
         contactAction = $("#contact"),
         contactClose = $(".form-close"),
         check = (".check"),
         checkInput = (".check input"),
-        contactBox = $(".contact");
+        contactBox = $(".contact"),
+        bxsliderServices = null;
+
 
    $(pull).on('click', function(e) {
-    e.preventDefault();
-    $(this).toggleClass(active);
-    $(nav).fadeToggle();
-    $(wrapper).fadeToggle();
+        e.preventDefault();
+        $(this).toggleClass(active);
+        $(nav).fadeToggle();
+        $(wrapper).fadeToggle();
+    });
+
+    $(window).resize(function(){
+        $(wrapper).fadeIn();
+        if(windowWidth < 768){
+           $(pull).on('click', function(e) {
+                e.preventDefault();
+                $(wrapper).fadeIn();
+            });
+        }
     });
 
    $(contactAction).on('click', function(e) {
     e.preventDefault();
     $(contactBox).fadeIn();
     $(nav).fadeOut();
+    $(pull).toggleClass(active);
     });
 
    $(contactClose).on('click', function() {
-    $(contactBox).fadeOut();
-    $(nav).fadeIn();
+        $(contactBox).fadeOut();
+        $(wrapper).fadeIn();
    });
 
     $(window).scroll(function() {
@@ -63,10 +76,9 @@ $(function() {
     });
 
     $(featuredWorkitem).each(function(){
-        var bgImg = $(this).attr("src");
-        console.log(bgImg);
+        var bgImg = $(this).find('img').attr("src");
         $(this).css("background-image", "url('" + bgImg + "')");
-        $(this).removeAttr("src");
+        $(this).find('img').remove();
     });
 
     $('.bx-slider').bxSlider({
@@ -80,6 +92,14 @@ $(function() {
             maxSlides: 1
         });
     }
+
+    if(windowWidth < 768){
+        bxsliderServices = $('#services').bxSlider({
+            minSlides: 1,
+            maxSlides: 1
+        });
+    }
+
 
     $('.works-slide').bxSlider({
         controls: true
