15183f648323b3fc71cd05defe3938251362385a
nabu1615 <AndresMac@MacBook-Pro-de-Javier.local>
2016-01-21 20:44:32 -0500

    desktop styling

--- js/main.js
+++ js/main.js
@@ -105,5 +105,22 @@ $(function() {
         controls: true
     });
 
+    if(windowWidth > 868){
+        $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target
+          || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top - 78;
+            $('html,body')
+            .animate({scrollTop: targetOffset}, 1000);
+           return false;
+          }
+        }
+        });
+    }
+
 });
 
